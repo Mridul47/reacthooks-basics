7d885da4d0042a6f75929697626484c42a2a9bb9
Mridul47 <mrkoirala58@gmail.com>
2022-08-23 12:58:33 +0545

    reacthooks practise

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import DataFetching from './DataFetching';
+
+function App() {
+  return (
+    <div className="App">
+      <DataFetching/>
+      
+    </div>
+  );
+}
+
+export default App;
